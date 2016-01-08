4c7e3810a9ed59ee0d8226d0d78e10bc1dea1a02
Jamie King <king0120@gmail.com>
2016-01-08 10:42:48 -0500

    random input working

--- server.rb
+++ server.rb
@@ -1,19 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 class Response
-  attr_accessor :compliments, :colors
-  @compliments = [
-    "Your instructors love you",
-    "High five = ^5",
-    "Is it Ruby Tuesday yet?",
-    "It's almost beer o'clock",
-    "The Force is strong with you"
-  ]
+  attr_accessor :compliments :colors
+  def initialize
+    @compliments = [
+      "Your instructors love you",
+      "High five = ^5",
+      "Is it Ruby Tuesday yet?",
+      "It's almost beer o'clock",
+      "The Force is strong with you"
+    ]
+  end
+
 
   @colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 end
 
+# binding.pry
+
 get '/' do
-  :compliments
+  compliment = Response.new
+  @compliment = compliment.compliments[rand(compliment.compliment.length)]
+  haml :compliment
 end
